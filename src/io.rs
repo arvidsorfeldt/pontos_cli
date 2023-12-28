f75e083aa4483c5b038f69c21471f9d947db07e4
arvidsorfeldt <arvid.sorfeldt@gmail.com>
2023-12-28 14:52:29 +0100

    Write file in folder

--- src/io.rs
+++ src/io.rs
@@ -1,15 +1,28 @@
 use chrono::NaiveDate;
 use csv::Writer;
 use futures::join;
+use std::fs::create_dir;
+use std::path::Path;
 
 use crate::data::{get_other_data, get_vessel_ids, get_vessel_position_data};
 
 fn output_csv<T: serde::Serialize>(
+    vessel_id: &str,
     date: NaiveDate,
-    parameter: String,
+    parameter: &str,
     list: Vec<T>,
 ) -> Result<(), Box<dyn std::error::Error>> {
-    let mut writer = Writer::from_path(parameter + "_" + &date.to_string() + ".csv").unwrap();
+    let mut writer = Writer::from_path(
+        vessel_id.to_string()
+            + "_"
+            + &date.to_string()
+            + "/"
+            + parameter
+            + "_"
+            + &date.to_string()
+            + ".csv",
+    )
+    .unwrap();
     for record in list {
         writer.serialize(record)?;
     }
@@ -22,13 +35,14 @@ pub async fn day_to_csv(
     vessel_id: &str,
     date: NaiveDate,
 ) -> Result<(), Box<dyn std::error::Error>> {
+    create_dir(Path::new(".").join(vessel_id.to_string() + "_" + &date.to_string()))?;
     let positions = get_vessel_position_data(vessel_id, date);
     let other_data = get_other_data(vessel_id, date);
     let (positions, other_data) = join!(positions, other_data);
-    output_csv(date, "position".to_string(), positions?)?;
+    output_csv(vessel_id, date, "position", positions?)?;
 
     for (p, data) in other_data? {
-        output_csv(date, p.as_str_short().to_string(), data)?;
+        output_csv(vessel_id, date, p.as_str_short(), data)?;
     }
 
     Ok(())
